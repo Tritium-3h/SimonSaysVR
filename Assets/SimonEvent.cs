a4704fc2040409c1c4d19daeead2e557b2478071
joaobiriba <glaquidara@gmail.com>
2017-03-28 17:01:55 +0200

    Use new logic with counter to prevent unwanted updates and allow same led liting; Add new sounds

---
+++ Assets/SimonEvent.cs
@@ -0,0 +1,17 @@
+﻿
+public class SimonEvent  {
+
+	public string color;
+	public string counter;
+
+	public SimonEvent(string color, string counter) {
+		this.color = color;
+		this.counter = counter;
+	}
+
+	public SimonEvent(string color, long counter) {
+		this.color = color;
+		this.counter = ""+counter;
+	}
+
+}
