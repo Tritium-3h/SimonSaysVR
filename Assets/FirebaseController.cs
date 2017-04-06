eb96613f6c0acfd7e687731d36f9e486b911383d
joaobiriba <glaquidara@gmail.com>
2017-04-06 15:58:58 +0200

    Using SimonEvent to update firebase remote db

--- Assets/FirebaseController.cs
+++ Assets/FirebaseController.cs
@@ -11,20 +11,28 @@ using UnityEngine.UI;
 
 using GVR.Entity;
 
+
 // Handler for UI buttons on the scene.  Also performs some
 // necessary setup (initializing the firebase app, etc) on
 // startup.
 public class FirebaseController : MonoBehaviour {
 
-	ArrayList leaderBoard;
-	Vector2 scrollPosition = Vector2.zero;
-	private Vector2 controlsScrollViewVector = Vector2.zero;
 
-	const int kMaxLogSize = 16382;
 	DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
 
 	public CollisionSoundPlayer key_one;
 	public CollisionSoundPlayer key_two;
+	public CollisionSoundPlayer key_three;
+	public CollisionSoundPlayer key_four;
+
+
+	private SoundPlayerBase pipePlayer;
+
+	public AudioClip[] soundFiles;
+
+	public AudioSource audioSource;
+
+	private long counter = 0;
 
 	// When the app starts, check to make sure that we have
 	// the required dependencies to use Firebase, and if not,
@@ -49,39 +57,8 @@ public class FirebaseController : MonoBehaviour {
 	// Initialize the Firebase database:
 	void InitializeFirebase() {
 		FirebaseApp app = FirebaseApp.DefaultInstance;
-		app.SetEditorDatabaseUrl("https://unitytest-72cdd.firebaseio.com/");
+		app.SetEditorDatabaseUrl("https://simonsaysfirebase.firebaseio.com/");
 		if (app.Options.DatabaseUrl != null) app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
-
-
-		/*leaderBoard = new ArrayList();
-		leaderBoard.Add("Firebase Top " + MaxScores.ToString() + " Scores");
-		FirebaseDatabase.DefaultInstance
-			.GetReference("Leaders").OrderByChild("score")
-			.ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
-			if (e2.DatabaseError != null) {
-				Debug.LogError(e2.DatabaseError.Message);
-				return;
-			}
-			string title = leaderBoard[0].ToString();
-			leaderBoard.Clear();
-			leaderBoard.Add(title);
-			if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0) {
-				foreach (var childSnapshot in e2.Snapshot.Children) {
-					DebugLog("update");
-
-					if (childSnapshot.Child("score") == null
-						|| childSnapshot.Child("score").Value == null) {
-						Debug.LogError("Bad data in sample.  Did you forget to call SetEditorDatabaseUrl with your project id?");
-						break;
-					} else {
-						leaderBoard.Insert(1, childSnapshot.Child("score").Value.ToString()
-							+ "  " + childSnapshot.Child("email").Value.ToString());
-						DebugLog("update " + childSnapshot.Child("email").Value.ToString());
-
-					}
-				}
-			}
-		};*/
 
 		FirebaseDatabase.DefaultInstance
 			.GetReference ("led")
@@ -94,15 +71,50 @@ public class FirebaseController : MonoBehaviour {
 
 			if (e2.Snapshot != null) {
 
-				Debug.Log("Joao " + e2.Snapshot.GetValue(false).ToString() );
+				switch (e2.Snapshot.Child("color").GetValue(false).ToString())
+				{
+				case "0":
+					key_one.PlaySound();
+					break;
+				case "1":
+					key_two.PlaySound();
+					break;
+				case "2":
+					key_three.PlaySound();
+					break;
+				case "3":
+					key_four.PlaySound();
+					break;
+				default:
+					Console.WriteLine("Default case");
+					break;
+				}
+
+			}
+		};
+
+
+		FirebaseDatabase.DefaultInstance
+			.GetReference ("status")
+			.ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+			if (e2.DatabaseError != null) {
+				Debug.LogError (e2.DatabaseError.Message);
+				return;
+			}
+
+
+			if (e2.Snapshot != null) {
+
 
 				switch (e2.Snapshot.GetValue(false).ToString())
 				{
-				case "red":
-					key_one.PlaySound();
+				case "lose":
+					audioSource.clip = soundFiles[0];
+					audioSource.Play();
 					break;
-				case "blue":
-					key_two.PlaySound();
+				case "win":
+					audioSource.clip = soundFiles[1];
+					audioSource.Play();					
 					break;
 				default:
 					Console.WriteLine("Default case");
@@ -115,10 +127,13 @@ public class FirebaseController : MonoBehaviour {
 
 
 	public void keyPressed(String key) {
+		counter++;
+		SimonEvent keyPressedEvent = new SimonEvent(key, counter);
+		string json = JsonUtility.ToJson(keyPressedEvent);
+
 		FirebaseDatabase.DefaultInstance
-			.GetReference ("key")
-			.SetValueAsync (key);
-		
+			.GetReference ("key").SetRawJsonValueAsync(json);
+
 	}
 
 
